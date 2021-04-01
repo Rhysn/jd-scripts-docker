afbbcf4b99e6bde8fd98c36e69095d4119ba9850
Rhysn <cedaren@gmail.com>
2021-04-01 17:18:49 +0800

    添加自动更新share code脚本

--- _Shell/help/Carnivalcity/get_jd_carnivalcity_ShareCode.sh
+++ _Shell/help/Carnivalcity/get_jd_carnivalcity_ShareCode.sh
@@ -4,7 +4,7 @@ num=0
 sharecodes=""
 for item in "$@"; do
     path="../../../logs/logs${item}/jd_carnivalcity.log"
-    sharecode=$(cat ${path} | grep '【京东账号1（.+）的京东手机狂欢城好友互助码】' | sed 's/【京东账号1（.+）的京东手机狂欢城好友互助码】//g')
+    sharecode=$(cat ${path} | grep '【京东账号1（' | sed 's/【京东账号1（.*）的京东手机狂欢城好友互助码】//g')
     if [ $num -eq 0 ]
     then
         sharecodes="${sharecode}"
@@ -15,4 +15,4 @@ for item in "$@"; do
 done
 
 sharecodes_str="JD818_SHARECODES=${sharecodes}"
-echo ${sharecodes_json} > jd_818
+echo ${sharecodes_str} > jd_818
