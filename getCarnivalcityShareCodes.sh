7dfcea332d83db4c22dfe8e41e46ed031cf9816c
Rhysn <cedaren@gmail.com>
2021-04-11 14:59:07 +0800

    .

--- getCarnivalcityShareCodes.sh
+++ getCarnivalcityShareCodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s -S -L https://rules.allgreat.xyz/Scripts/JD/InviteCodes/jd_818 > /jd-scripts-docker/env/Temp_ShareCodes.sh
+curl -s -S -L https://rules.allgreat.xyz/Scripts/JD/InviteCodes/jd_818 > /jd-scripts-docker/env/jd_818
